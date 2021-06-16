82984682d26c702749e1107e61ccf434faa1f55c
jabtem <suchie777@gmail.com>
2021-06-16 12:28:09 +0900

    에디터 커스텀시도 이름지정해서 프리펩생성하는것 성공 캐릭생성메뉴로 수정할계획

---
+++ Assets/Editor/CreateCharacter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateCharacter : ScriptableWizard
+{
+    // static 선언 안하면 에러
+    static string str;
+    static GameObject go;
+
+    [MenuItem("Create/Character")]
+    static void CreateWindow()
+    {
+        DisplayWizard<CreateCharacter>("캐릭터 생성");
+        go = new GameObject();
+        //EditorWindow.GetWindow(typeof(CreateCharacter));
+    }
+
+    //void OnGUI()
+    //{
+    //    GUILayout.Label("프리펩 이름");
+    //    str = EditorGUILayout.TextField(str);
+
+    //    string prefabPath = "Assets/Prefabs/MyPrefabs/"+str+".prefab";
+    //    if (GUILayout.Button("Create"))
+    //    {
+    //        PrefabUtility.SaveAsPrefabAsset(test, prefabPath);
+    //    }
+    //}
+
+    protected override bool DrawWizardGUI()
+    {
+        GUILayout.Label("프리펩 이름");
+        str = EditorGUILayout.TextField(str);
+        return true;
+    }
+
+    private void OnWizardCreate()
+    {
+        string prefabPath = "Assets/04.Prefabs/Character/" + str +".prefab";
+        PrefabUtility.SaveAsPrefabAsset(go, prefabPath);
+        GameObject.DestroyImmediate(go);
+    }
+}
